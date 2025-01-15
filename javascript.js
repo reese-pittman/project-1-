3e66bada71552a96f0804fa152c86923103d9b80
Reese Pittan <reeseaverypittman@gmail.com>
2025-01-15 14:54:03 -0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,37 @@
+
+
+async function getTopAnime(count = 5) {
+    const imageContainer = document.querySelector("#top-shows")
+    const url = `https://api.jikan.moe/v4/top/anime?sfw=true&limit=${count}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        let count = 0;
+        const json = await response.json();
+        json.data.forEach(element => {
+            console.log(element.images.jpg.image_url)
+            let imgtag = document.createElement("img");
+            imgtag.src = element.images.jpg.image_url
+            count++;
+
+            if (count > 1){
+                
+            }
+
+            imageContainer.appendChild(imgtag);
+            
+
+            
+        });
+    }catch (error) {
+        console.error(error.message);
+    }
+    
+};
+
+
+

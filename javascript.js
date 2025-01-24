19fa7bed7610b6cb54136281782526e2ef8204bd
Reese Pittan <reeseaverypittman@gmail.com>
2025-01-24 17:49:31 -0600

    Added img dusplay feature that correctly displays images.

--- javascript.js
+++ javascript.js
@@ -1,37 +1,48 @@
 
+let anime_continue_list = []; 
+let centerIndex = 1;
 
 async function getTopAnime(count = 5) {
-    const imageContainer = document.querySelector("#top-shows")
     const url = `https://api.jikan.moe/v4/top/anime?sfw=true&limit=${count}`;
+    
 
     try {
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
         }
-
-        
         const json = await response.json();
-        json.data.forEach(element => {
-            console.log(element.images.jpg.image_url)
-            let imgtag = document.createElement("img");
-            imgtag.src = element.images.jpg.image_url
-            imgtag.classList.add("top-anime-img")
-
-            if (count > 1){
-                
-            }
-
-            imageContainer.appendChild(imgtag);
-            
-
-            
-        });
+        
+        return json.data.map(element => element.images.jpg.image_url); 
+        
     }catch (error) {
         console.error(error.message);
     }
     
 };
 
+async function displayImages() {
+    const imageContainer = document.querySelector("#watched-shows")
 
+    const imgUrls = await getTopAnime();
 
+    anime_continue_list = imgUrls.map(img_url => {
+        let imgtag = document.createElement("img");
+        imgtag.src = img_url;
+        imgtag.classList.add("hidden-img");
+
+        imageContainer.appendChild(imgtag);
+        return imgtag;
+    });
+
+    showImgs();
+
+};
+
+function showImgs(){
+    anime_continue_list[centerIndex].classList.add("current-img");
+    anime_continue_list[centerIndex + 1].classList.add("side-img");
+    anime_continue_list[centerIndex - 1].classList.add("side-img");
+
+    anime_continue_list.slice(0, 3).forEach(element => element.classList.remove("hidden-img"));
+};

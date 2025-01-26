77a9f91176538408143327df8684368aa021339c
Reese Pittan <reeseaverypittman@gmail.com>
2025-01-25 18:46:11 -0600

    The carasal feature has been implemented and working

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 
 let anime_continue_list = []; 
+let leftIndex = 0
 let centerIndex = 1;
+let rightIndex = 2
+let imgUrls = [];
+let imgList = [];
+
+
 
 async function getTopAnime(count = 5) {
     const url = `https://api.jikan.moe/v4/top/anime?sfw=true&limit=${count}`;
@@ -22,27 +28,72 @@ async function getTopAnime(count = 5) {
 };
 
 async function displayImages() {
-    const imageContainer = document.querySelector("#watched-shows")
+    const imageContainer = document.querySelector("#watched-shows");
+    const centerImg = imageContainer.querySelector("#centerImg");
+    const rightImg = imageContainer.querySelector("#rightImg");
+    const leftImg = imageContainer.querySelector("#leftImg");
 
-    const imgUrls = await getTopAnime();
+    imgList = [leftImg, centerImg, rightImg];
 
-    anime_continue_list = imgUrls.map(img_url => {
-        let imgtag = document.createElement("img");
-        imgtag.src = img_url;
-        imgtag.classList.add("hidden-img");
-
-        imageContainer.appendChild(imgtag);
-        return imgtag;
-    });
-
-    showImgs();
+    imgUrls = await getTopAnime();
+    try {
+        for (let i = 0; i < 3; i++){
+            imgList[i].src = imgUrls[i];
+        }
+        console.log(imgUrls);
+    } catch(err) {
+        console.log(err);
+    }
+    
+    formatImgs();
 
 };
 
-function showImgs(){
-    anime_continue_list[centerIndex].classList.add("current-img");
-    anime_continue_list[centerIndex + 1].classList.add("side-img");
-    anime_continue_list[centerIndex - 1].classList.add("side-img");
 
-    anime_continue_list.slice(0, 3).forEach(element => element.classList.remove("hidden-img"));
-};
+function rightImg(){
+
+
+    centerIndex++;
+    rightIndex++;
+    leftIndex++;
+    formatImgs();
+
+}
+
+function leftImg() {
+    centerIndex--;
+    rightIndex--;
+    leftIndex--;
+    formatImgs();
+}
+
+
+function calculateIdexPos() {
+    if (centerIndex >= imgUrls.length){
+        centerIndex = 0;
+    } else if (centerIndex < 0){
+        centerIndex = imgUrls.length - 1;
+    }
+    
+    if (rightIndex >= imgUrls.length) {
+        rightIndex = 0;
+    } else if (rightIndex < 0) {
+        rightIndex = imgUrls.length - 1;
+    }
+
+    if (leftIndex >= imgUrls.length) {
+        leftIndex = 0;
+    } else if (leftIndex < 0) {
+        leftIndex = imgUrls.length - 1;
+    }
+}
+
+function formatImgs(){
+    calculateIdexPos();
+   
+    imgList[0].src = imgUrls[leftIndex];
+    imgList[1].src = imgUrls[centerIndex];
+    imgList[2].src = imgUrls[rightIndex];
+
+
+}
